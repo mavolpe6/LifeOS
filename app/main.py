11de04227163152aeba91685fb27dbd4bdc11a14
mavolpe6 <mavolpe@unc.edu>
2026-06-29 23:22:03 -0400

    add habbits synched to API

--- app/main.py
+++ app/main.py
@@ -8,3 +8,10 @@ app = FastAPI()
 class Habit(BaseModel):
     name: str
 
+@app.post("/habits")
+ def create_habit(new_habit: Habit):
+     new_habbit_string= new_habit.name
+    add_habit(new_habbit_string)
+    return {"message": f"Habit {new_habbit_string} added"}
+
+
