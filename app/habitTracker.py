5902aa1e09a8827016bf0a0f425d0012075ec89b
mavolpe6 <mavolpe@unc.edu>
2026-06-29 23:41:51 -0400

    Delete habits and habit tracker fix

--- app/habitTracker.py
+++ app/habitTracker.py
@@ -3,22 +3,18 @@ def main():
     view_habits(habit_tracker)
 habit_tracker = []
 
-def add_habit(habit_tracker):
-    habit = input("Enter a habit: ")
-    habit_tracker.append(habit)
-    print(f" {habit} added to tracker")
+def add_habit(added_habit:string):
+    habit_tracker.append(added_habit)
+    print(f" {added_habit} added to tracker")
 
-def view_habits(habit_tracker):
-    print("Habits:")
-    for habit in habit_tracker:
-        print(f"- {habit}")
-def remove_habit(habit_tracker):
-    habit = input("Enter a habit to remove: ")
-    if habit in habit_tracker:
-        habit_tracker.remove(habit)
-        print(f" {habit} removed from tracker")
+def view_habits():
+    return habit_tracker
+def remove_habit(removed_habit:string):
+    if removed_habit in habit_tracker:
+        habit_tracker.remove(removed_habit)
+        print(f" {removed_habit} removed from tracker")
     else:
-        print(f" {habit} not found in tracker")  
+        print(f" {removed_habit} not found in tracker")  
 def edit_habit(habit_tracker):
     habit = input("Enter a habit to edit: ")
     if habit in habit_tracker:
--- app/main.py
+++ app/main.py
@@ -10,11 +10,14 @@ class Habit(BaseModel):
 #create a habit
 @app.post("/habits")
  def create_habit(new_habit: Habit):
-     new_habbit_string= new_habit.name
-    add_habit(new_habbit_string)
-    return {"message": f"Habit {new_habbit_string} added"}
+     new_habit_string= new_habit.name
+    add_habit(new_habit_string)
+    return {"message": f"Habit {new_habit_string} added"}
 #get all habits
 @app.get("/habits")
 def get_all_habits():
     return{view_habits()}
-
+@app.delete("/habits/{habit_name}")
+def delete_habit(habit_name:str):
+   return remove_habit(habit_name)
+    
