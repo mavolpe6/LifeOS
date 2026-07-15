aef5c7b0e2f350fe66cab9aec25c3086f4076183
mavolpe6 <mavolpe@unc.edu>
2026-07-14 22:07:47 -0400

    Bug fixes on main

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from habbitTracker import *
 from fastapi.middleware.cors import CORSMiddleware
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -10,7 +11,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app = FastAPI()
+
 
 class Habit(BaseModel):
     name: str
@@ -18,18 +19,18 @@ class Habit(BaseModel):
     streak: int
 #get all habits
 @app.post("/habits")
-def add_habit(new_habit:Habit):
+def create_habit(new_habit:Habit):
     return add_habit(new_habit)
 @app.get("/habits")
 def get_all_habits():
-    return{view_habits()}
+    return view_habits()
 @app.delete("/habits/{habit_name}")
 def delete_habit(habit_name:str):
    return remove_habit(habit_name)
 @app.put("/habits/{habit_name}")
 def update_habit(habit_name:str, new_habit:Habit):
     return edit_habit(habit_name, new_habit.name)
-@app.get("/habits/{habit_name}")
+@app.get("/habits/habit_name")
 def get_habit(habit_name:str):
     return get_habit(habit_name)
     
