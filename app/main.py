f69b6104a12c7f81df5d2dffa4b0a2c156d4ca96
mavolpe6 <mavolpe@unc.edu>
2026-07-15 22:56:09 -0400

    pydantic fix

--- app/main.py
+++ app/main.py
@@ -20,7 +20,8 @@ class Habit(BaseModel):
 #get all habits
 @app.post("/habits")
 def create_habit(new_habit:Habit):
-    return add_habit(new_habit)
+    news_habit=new_habit.model_dump()
+    return add_habit(news_habit)
 @app.get("/habits")
 def get_all_habits():
     return view_habits()
@@ -29,7 +30,9 @@ def delete_habit(habit_name:str):
    return remove_habit(habit_name)
 @app.put("/habits/{habit_name}")
 def update_habit(habit_name:str, new_habit:Habit):
-    return edit_habit(habit_name, new_habit.name)
+    news_habit=new_habit.model_dump()
+    habits_name=habit_name.model_dump()
+    return edit_habit(habits_name, news_habit.name, news_habit.description)
 @app.get("/habits/habit_name")
 def get_habit(habit_name:str):
     return get_habit(habit_name)
