276fd28ca40d2b5f9a66294082f9a8943579f207
mavolpe6 <mavolpe@unc.edu>
2026-07-30 05:48:26 -0400

    bug fix 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from habitTracker import add_habit, view_habits, remove_habit, edit_habit, get_habit
+from habitTracker import HabitTracker
 from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
